72c0d822830c8ed24935d0c43242514f512d04df
Shane Mingins <smingins@Shane-Minginss-MacBook-Pro.local>
2011-01-10 17:49:17 +1300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Finman::Application.config.secret_token = '6aed09005e37fca5d99fd9c879d6bc91bfcc846826b4816688d0b8647e207646eea98807e0dd2c67db0db5af2362b54d45feba3037c6f795678b6cbc155b59c0'
